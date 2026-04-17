d43c5bb2ba2b32180b5ab24a79c2f8fa65a65efb
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-04-17 11:26:44 -0600

    000 Primer commit del proyecto

---
+++ src/org/javiersian/system/Principal.java
@@ -0,0 +1,12 @@
+
+package org.javiersian.system;
+
+/**
+ *
+ * @author Javier Sian
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo estoy en Kinal");
+    }
+}
